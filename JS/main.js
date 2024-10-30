d80a56412544d155166c87d1b4db8e64aef89202
Juan Malaver <huanxe.tech@gmail.com>
2024-10-30 11:51:32 -0500

    Arreglar barra de navegación según issue

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,24 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    const commentToggles = document.querySelectorAll('.comment-toggle');
-    const addCommentButtons = document.querySelectorAll('.add-comment');
-  
-    commentToggles.forEach(button => {
-      button.addEventListener('click', function() {
-        const commentsSection = this.nextElementSibling;
-        commentsSection.style.display = commentsSection.style.display === 'none' ? 'block' : 'none';
-      });
-    });
-  
-    addCommentButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        const input = this.previousElementSibling;
-        const commentsList = input.nextElementSibling;
-        const newComment = document.createElement('li');
-        newComment.textContent = input.value;
-        commentsList.appendChild(newComment);
-        input.value = '';
-      });
+  const commentToggles = document.querySelectorAll('.comment-toggle');
+  const addCommentButtons = document.querySelectorAll('.add-comment');
+
+  commentToggles.forEach(button => {
+    button.addEventListener('click', function() {
+      const commentsSection = this.nextElementSibling;
+      commentsSection.style.display = commentsSection.style.display === 'none' ? 'block' : 'none';
     });
   });
-  
+
+  addCommentButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const input = this.previousElementSibling;
+      const commentsList = input.nextElementSibling;
+      const newComment = document.createElement('li');
+      newComment.textContent = input.value;
+      commentsList.appendChild(newComment);
+      input.value = '';
+    });
+  });
+});
+
